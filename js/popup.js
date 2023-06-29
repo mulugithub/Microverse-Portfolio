072859cbafad1f02d790dd8ecedd8f03828b1c4e
Muluken Zemed <zmmulu@gmail.com>
2023-06-29 16:46:45 +0300

    changes on description container to set poput-left and popup right div for desktop version.

--- js/popup.js
+++ js/popup.js
@@ -15,7 +15,7 @@ const projects = [
       './images/Snapshoot Portfolio-desk-1.png',
     ],
     technologies: [
-      ['html', 'css', 'JavaScript'], [''],
+      ['html', 'css', 'JavaScript'], ['Ruby on rails', 'Ruby', 'Bootstrap'],
     ],
     client: ['CANOPY', 'CANOPY'],
     roles: ['./images/Counter.png', ['Back End Dev', 'Back End Dev'], ['2015', '2015']],
@@ -109,7 +109,7 @@ main.insertBefore(workSection, main.children[1]);
 
 // Create projectCards dynamically using the information in the projects array
 projects.forEach((project, index) => {
-  const projectCards= document.createElement('div');
+  const projectCards = document.createElement('div');
   projectCards.setAttribute('class', 'cards-container');
   projectCards.classList.add('project');
   projectCards.innerHTML = `
@@ -146,3 +146,84 @@ projects.forEach((project, index) => {
 // select and  hide  the second li item from the class tags for desktop version
 const hideSecondTechnology = document.querySelector('.left-block .tags li:nth-child(2)');
 hideSecondTechnology.style.display = 'none';
+
+// Get the popup-modal-container class
+const popupModal = document.querySelector('.popup-modal-container');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = () => {
+  popupModal.classList.toggle('hide');
+  overlay.classList.toggle('hide');
+  body.style.overflow = 'auto';
+};
+
+const showModal = () => {
+  popupModal.classList.toggle('hide');
+  overlay.classList.toggle('hide');
+  body.style.overflow = 'hidden';
+};
+
+// Define functions to show the popup modal
+function showPopupModal(projectId) {
+  popupModal.innerHTML = `
+  <div class='modal-header'>
+   <h2 class="project-title mobile" >${projects[projectId].name[0]}</h2>
+   <h2 class="project-title desktop">${projects[projectId].name[1]}</h2>
+   <a class='close-modal'>&times; </a>
+  </div>
+  <div class='modal-content'>
+    <ul class="project-detail">
+      <li class="card-client mobile">${projects[projectId].client[0]}</li>
+      <li class="card-client desktop">${projects[projectId].client[1]}</li>
+      <li class="card-role"><img src=" ${projects[projectId].roles[0]}" alt="counter" /></li>
+      <li class="card-role mobile"> ${projects[projectId].roles[1][0]}</li>
+      <li class="card-role desktop">${projects[projectId].roles[1][1]}</li>
+      <li class="card-role"><img src="${projects[projectId].roles[0]}" alt="counter" /></li>
+      <li class="card-role mobile">${projects[projectId].roles[2][0]}</li>
+      <li class="card-role desktop">${projects[projectId].roles[2][1]}</li>
+    </ul>
+  
+    <div class='snapshoot-popup mobile'> <img src="${projects[projectId].featured_images[0]}" alt="${projects[projectId].name[0]}"></div>
+    <div class='snapshoot-popup desktop'> <img src="${projects[projectId].featured_images[1]}" alt="${projects[projectId].name[1]}"></div>
+    
+    <div class="description-container">
+    <div class="popup-left">
+    <p class="description mobile">${projects[projectId].details[0]}</p>
+    <p class="description desktop">${projects[projectId].details[1][0]}</p>
+    <p class="description desktop">${projects[projectId].details[1][1]}</p>
+    </div>
+    <div class="popup-right">
+    <ul class="tags">
+      <li class="tags-button mobile">${projects[projectId].technologies[0][0]}</li>
+      <li class="tags-button mobile">${projects[projectId].technologies[0][1]}</li>
+      <li class="tags-button mobile">${projects[projectId].technologies[0][2]}</li>
+      <li class="tags-button desktop">${projects[projectId].technologies[1][1]}</li>
+      <li class="tags-button desktop">${projects[projectId].technologies[1][2]}</li>
+    </ul>
+
+    <img src="images/divider-mobile-modal.png" alt="divider image for mobile popup">
+    
+    <div class="live-source">
+    <a class='live-soucre-btn' href='https://${projects[projectId].liveLink}' target='_blank'>See Live <img src='./images/Icon-Live.png' alt='Live link icon'></a>
+    <a class='live-soucre-btn' href='https://${projects[projectId].sourceLink}' target='_blank'>See Source <img src='./images/Icon-GitHub.png' alt='Github icon'></a>
+    </div>
+    </div>
+
+    </div>
+    </div>
+  `;
+}
+
+const projectDetailClickHandler = (projectId) => {
+  showPopupModal(projectId);
+  showModal();
+  document.querySelector('.close-modal').addEventListener('click', closeModal);
+};
+
+const popupBtns = document.querySelectorAll('.project-action');
+popupBtns.forEach((btn) => {
+  const projectId = btn.getAttribute('data-project-id');
+  btn.addEventListener('click', () => projectDetailClickHandler(projectId));
+});
+
+overlay.addEventListener('click', closeModal);
